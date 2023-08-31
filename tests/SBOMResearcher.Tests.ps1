5138f85d7140b9d5f373d7005d53f7ffe51850d7
Kurt Hockaday <bigdawgsfootball@gmail.com>
2023-08-31 17:56:17 -0400

    add a pester test

---
+++ tests/SBOMResearcher.Tests.ps1
@@ -0,0 +1,22 @@
+# Import the function to test
+. ..\SBOMResearcher.ps1
+
+# Define the test cases
+$testCases = @(
+    @{High = "UNSET"; Compare = "UNSET"; Expected = "Unresolved version"}
+    @{High = "UNSET"; Compare = "1.0.0"; Expected = "1.0.0"}
+    @{High = "1.0.0"; Compare = "UNSET"; Expected = "1.0.0"}
+    @{High = "1.0.0"; Compare = "2.0.0"; Expected = "2.0.0"}
+    @{High = "2.0.0"; Compare = "1.0.0"; Expected = "2.0.0"}
+    @{High = "Unresolved version"; Compare = "UNSET"; Expected = "Unresolved version"}
+    @{High = "Unresolved version"; Compare = "1.0.0"; Expected = "Unresolved version"}
+    @{High = "1.0.0"; Compare = "Invalid"; Expected = "Unresolved version"}
+)
+
+# Run the tests
+Describe 'Get-HighVersion' {
+    It 'returns <Expected> when High is <High> and Compare is <Compare>' -TestCases $testCases {
+        param($High, $Compare, $Expected)
+        Get-HighVersion -High $High -Compare $Compare | Should -Be $Expected
+    }
+}
